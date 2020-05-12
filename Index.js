61dd3a86ffaf0946d7b64f0b04ba31b0e98117d1
AntonBundgaard <49413836+AntonBundgaard@users.noreply.github.com>
2020-05-12 13:12:11 +0200

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "Hej Anton!";
+    document.getElementById("demo2").innerHTML = "Hvordan går det?";
+    window.open('http://www.youtube.com')
+    
+}
